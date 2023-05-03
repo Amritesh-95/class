4cc0a1ecb709a49ccaf9f7a24abbd680ab05942e
Amritesh-95 <115081513+Amritesh-95@users.noreply.github.com>
2023-05-03 11:42:18 +0530

    prac

---
+++ pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i=1,j;
+    for(i;i<=9;i++)
+    {
+        j=1;
+        for(j;j<=i;j++){
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+return 0;
+}
